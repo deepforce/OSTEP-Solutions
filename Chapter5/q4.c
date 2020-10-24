1bcb16cf41f3cec8d4ea7899fd77be28605ece65
Zack <yuqz95@gmail.com>
2020-10-24 03:31:53 -0700

    Finish q4 to q7

---
+++ Chapter5/q4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int rc = fork();
+
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        char *argvs[] = {"ls", "-l", "-a", "-h", NULL};
+        char *envp[] = {NULL};
+
+        execl("/bin/ls", "ls", "-l", "-a", "-h", NULL);
+        execlp("ls", "ls", "-l", "-a", "-h", NULL);
+        execle("/bin/ls", "ls", "-l", "-a", "-h", NULL, envp);
+
+        execv("/bin/ls", argvs);
+        execvp("ls", argvs);
+    }
+    return 0;
+}
